762edef71fcbb30569722cf40678f67d73c19760
Davide Di Criscito <dcdavidev@gmail.com>
2026-03-13 11:45:09 +0100

    ci: disable lefthook during release

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -7,4 +7,4 @@ git config user.email "github-actions[bot]@users.noreply.github.com"
 
 # Run release-it in CI mode (non-interactive)
 # It will use the configuration from .release-it.json
-pnpm release-it --ci
+LEFTHOOK="0" pnpm release-it --ci
